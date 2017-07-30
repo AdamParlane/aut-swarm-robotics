7079e0fbdd006e8c08d08c040ab8cbf7ca64f2d8
wittsend <wittsend86@gmail.com>
2017-07-30 16:40:45 +1200

    Slowed twi0 to 50khz for testing

--- RobotNoASF/twimux_interface.c
+++ RobotNoASF/twimux_interface.c
@@ -66,17 +66,17 @@ void twi0Init(void)
 	twi0Reset;								//Software reset
 
 	//TWI0 Clock Waveform Setup
+	REG_TWI0_CWGR
+	|=	TWI_CWGR_CKDIV(4)					//Clock speed 400000, fast mode
+	|	TWI_CWGR_CLDIV(73)					//Clock low period 1.5uSec
+	|	TWI_CWGR_CHDIV(48);					//Clock high period  1.0uSec
+	twi0MasterMode;							//Master mode enabled, slave disabled
+
 	//REG_TWI0_CWGR
 	//|=	TWI_CWGR_CKDIV(1)					//Clock speed 400000, fast mode
-	//|	TWI_CWGR_CLDIV(73)					//Clock low period 1.5uSec
-	//|	TWI_CWGR_CHDIV(48);					//Clock high period  1.0uSec
+	//|	TWI_CWGR_CLDIV(148)					//Clock low period 1.5uSec
+	//|	TWI_CWGR_CHDIV(98);					//Clock high period  1.0uSec
 	//twi0MasterMode;							//Master mode enabled, slave disabled
-
-	REG_TWI0_CWGR
-	|=	TWI_CWGR_CKDIV(1)					//Clock speed 400000, fast mode
-	|	TWI_CWGR_CLDIV(148)					//Clock low period 1.5uSec
-	|	TWI_CWGR_CHDIV(98);					//Clock high period  1.0uSec
-	twi0MasterMode;							//Master mode enabled, slave disabled
 }
 
 /*
