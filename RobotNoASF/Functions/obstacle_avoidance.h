82b0f8f69ecd9a09c20fbd4612553162bda2484e
Matt Witt <wittsend@users.noreply.github.com>
2017-08-08 15:43:14 +1200

    Add obstacle_avoidance functions.
    Added scanProxSensors() function which simply reads the values from all of the proximity sensors and returns a 1 if something is found. Will be used to tell the main function when to switch to 'obstacle avoidance' state (yet to be implemented)

---
+++ RobotNoASF/Functions/obstacle_avoidance.h
@@ -0,0 +1,31 @@
+/*
+* obstacle_avoidance.h
+*
+* Author : Matthew Witt (pxf5695@autuni.ac.nz)
+* Created: 8/08/2017 3:07:52 PM
+*
+* Project Repository: https://github.com/AdamParlane/aut-swarm-robotics
+*
+* Function protoypes for obstacle avoidance system
+*
+* More Info:
+* Atmel SAM 4N Processor Datasheet:http://www.atmel.com/Images/Atmel-11158-32-bit%20Cortex-M4-Microcontroller-SAM4N16-SAM4N8_Datasheet.pdf
+* Relevant reference materials or datasheets if applicable
+*
+* Functions:
+* uint8_t scanProxSensors(uint8_t obstacleDetected)
+*
+*/
+
+#ifndef OBSTACLE_AVOIDANCE_H_
+#define OBSTACLE_AVOIDANCE_H_
+///////////////Includes/////////////////////////////////////////////////////////////////////////////
+#include "../robot_defines.h"
+
+///////////////Functions////////////////////////////////////////////////////////////////////////////
+uint8_t scanProxSensors(uint8_t obstacleDetected);
+
+
+
+
+#endif /* OBSTACLE_AVOIDANCE_H_ */
